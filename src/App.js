f6638ba11f996f50b772924f5ffd047762de261f
PYN <yxp7531540@gmail.com>
2019-06-07 14:50:12 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+import React, {PureComponent} from 'react'
+
+export default class App extends PureComponent {
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(res => res.json())
+        .then(obj => {
+            return obj
+        })
+    }
+}
