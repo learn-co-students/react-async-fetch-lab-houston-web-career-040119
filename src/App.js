45a09f215db4a67836c5d85182165ce4c89be3ec
Ubuntu <ubuntu@ip-172-31-91-199.ec2.internal>
2019-06-11 21:39:04 +0000

    done but no test

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React from "react";
+
+export default class App extends React.Component {
+    contructor() {
+        super()
+        this.state = {
+            peopleInSpace: null
+        }
+    }
+
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json").then(res => res.json()).then(data => {
+            this.setState({
+                peopleInSpace: data
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+
+}
